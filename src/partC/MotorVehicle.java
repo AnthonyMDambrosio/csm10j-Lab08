414809518125173796d200ddd89da6945a6e22dd
AnthonyMDambrosio <anthonymdambrosio@gmail.com>
2015-10-19 17:49:16 -0700

    Create MotorVehicle.java

---
+++ src/partC/MotorVehicle.java
@@ -0,0 +1,27 @@
+/****************************************
+Programmer : Anthony D'Ambrosio
+Title      : Lab08
+Date       : 10/19/2015
+*****************************************/
+package partC;
+
+public abstract class MotorVehicle
+{
+    private String name;
+    
+    public MotorVehicle ( String name )
+    {
+        this.name = name;   
+    }
+    
+    public String getName()
+    {
+        return name;
+    }
+    
+    public abstract void TurnOn();
+    
+    public abstract void TurnOff();
+
+    
+}

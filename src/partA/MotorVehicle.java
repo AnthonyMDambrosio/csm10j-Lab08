264de47f30419eb75cc7757f65cef79f5595a767
AnthonyMDambrosio <anthonymdambrosio@gmail.com>
2015-10-19 17:47:22 -0700

    Create MotorVehicle.java

---
+++ src/partA/MotorVehicle.java
@@ -0,0 +1,12 @@
+/****************************************
+Programmer : Anthony D'Ambrosio
+Title      : Lab08
+Date       : 10/19/2015
+*****************************************/
+package partA;
+
+public abstract class MotorVehicle
+{
+    public abstract void TurnOn();
+    public abstract void TurnOff();
+}
